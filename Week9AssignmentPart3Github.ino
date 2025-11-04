a012408411e6483f079f529fa61b060a1c555d67
msc10012 <msc10012@nyu.edu>
2025-11-04 13:05:46 -0500

    Initial commit

---
+++ Week9AssignmentPart3Github.ino
@@ -0,0 +1,41 @@
+#include "BetterButton.h"
+
+int button1Pin = 28;
+int button2Pin = 29;
+int button3Pin = 30;
+int button4Pin = 31;
+int midiNotes[4] = { 60, 62, 64, 65 };
+
+BetterButton button1(button1Pin, 0);
+BetterButton button2(button2Pin, 1);
+BetterButton button3(button3Pin, 2);
+BetterButton button4(button4Pin, 3);
+
+BetterButton* buttonArray[4] = { &button1, &button2, &button3, &button4 };
+
+void setup() {
+  Serial.begin(9600);
+
+  for (int i = 0; i < 4; i++) {
+    buttonArray[i]->pressHandler(onPress);
+    buttonArray[i]->releaseHandler(onRelease);
+  }
+}
+
+void loop() {
+  for(int i=0; i<4; i++) {
+    buttonArray[i]->process();
+  }
+}
+
+void onPress(int val) {
+  Serial.print(val);
+  Serial.println(" on");
+  usbMIDI.sendNoteOn(midiNotes[val], 127, 1);
+}
+
+void onRelease(int val) {
+  Serial.print(val);
+  Serial.println(" off");
+  usbMIDI.sendNoteOff(midiNotes[val], 0, 1);
+}
